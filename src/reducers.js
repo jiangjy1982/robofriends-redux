aa5783a29125217b39c4bd6cec921c13aac19289
Jiayan Jiang <jiangjy1982@gmail.com>
2021-03-06 17:55:25 -0800

    Minor changes.

--- src/reducers.js
+++ src/reducers.js
@@ -12,7 +12,7 @@ const initialStateSearch = {
 export const searchRobots = (state=initialStateSearch, action={}) => {
   switch (action.type) {
     case CHANGE_SEARCHFIELD:
-      return Object.assign({}, state, {searchField: action.payload})
+      return {...state, searchField: action.payload}
     default:
       return state
   }
@@ -26,11 +26,11 @@ const initialStateRobots = {
 export const requestRobots = (state=initialStateRobots, action={}) => {
   switch (action.type) {
     case REQUEST_ROBOTS_PENDING:
-      return Object.assign({}, state, {isPending: true})
+      return {...state, isPending: true}
     case REQUEST_ROBOTS_SUCCESS:
-      return Object.assign({}, state, {robots: action.payload, isPending: false})
+      return {...state, robots: action.payload, isPending: false}
     case REQUEST_ROBOTS_FAILED:
-      return Object.assign({}, state, {error: action.payload})
+      return {...state, error: action.payload}
     default:
       return state
   }
